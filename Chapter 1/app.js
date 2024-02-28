ace1430ba74c353875e828a28e7e5949827b641d
Ghulam Mustafa <60226955+ghulam-mustafaaa@users.noreply.github.com>
2024-02-29 04:40:15 +0500

    Add files via upload

---
+++ Chapter 1/app.js
@@ -0,0 +1,51 @@
+//*************************************************//
+//**************** Chapter 1 ***********************//
+//*************************************************//
+//An alert() is a jvascript built-in/pre-defined function that shows a popup message 
+//used to show some message or error etc.
+ 
+//Question 1
+    function one(){
+
+        alert("Welcome to SMIT");
+    }
+
+//Question 2
+    function two(){
+
+        alert("Error! Please Enter a valid passord");
+    }
+
+//Question 3
+    function three(){
+        alert("Welome to the world of js \n Happy Coding");
+    }
+
+//Question 4
+    function four(){
+        alert("Welome to the world of JS..");
+        alert("Happy Coding! "); //my broser did'nt show: "Prevent this page from creating additional dialogs"
+    }
+
+//Question 5
+function five(){
+            
+    alert("Hello..i can run js using my browser's console");
+    console.log("Hello..i can run JS using my browser's console"); //this will show message in browser's console 
+   // document.getElementById('img').style.display="block"; //displays screenshot of browsers console + output alert
+   
+    }
+    function show() {
+
+        var img = document.getElementById("myImage");
+        img.src="img/5.png";
+        img.style.display="block";
+        alert("show is running");
+    
+        }
+
+
+// Question 6
+    //used it already in an html file
+    //chapter 1/index.html
+
